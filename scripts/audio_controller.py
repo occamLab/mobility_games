8bd43b6210ada85fda6667810b2cbf60b168c3be
Jeremy <jeremychristopherryan@gmail.com>
2017-06-09 16:13:25 -0400

    Added audio functions, musical cane file.

--- scripts/audio_controller.py
+++ scripts/audio_controller.py
@@ -1,6 +1,11 @@
 from audiolazy import *
+import numpy as np
+import time
 
 def freq(name):
+    """ Generates frequency based on note name.
+    Example: freq("G4")"""
+
     try:
         letter = name[0]
         name = name[1:]
@@ -38,24 +43,72 @@ def delay(sig, delaynum, delayinterval, startamp):
   # or something alike (e.g. ensuring that duration outside of this
 # function), helping you to avoid an endless signal.
 
+def arrays_to_sound(list_of_tracks, quarter_time):
+    """ Takes in a list of numpy arrays for audio tracks and
+    the time of a quarter note, in seconds.
+
+    Returns a list of sound objects corresponding to the tracks."""
+    i = 0
+    sounds = []
+    master_array = np.concatenate(list_of_tracks, 1)
+    for chord in master_array:
+        chord_list = []
+        for note in chord:
+            chord_list.append(pns([[note]], quarter_time)[0])
+            i += 1
+        sounds.append(chord_list)
+    return np.asarray(sounds)
+
 def synth(freq):
+    """ Generates a karplus_strong sound at a given frequency."""
     rate = 44100 # Sampling rate, in samples/second
     s, Hz = sHz(rate) # Seconds and hertz
     ms = 1e-3 * s
-    return karplus_strong(freq)
+    return karplus_strong(freq * Hz)
+
+def player():
+    """ Generates the player object."""
+    return AudioIO(True)
+
+def play(sound, player):
+    """ Plays a sound object """
+    rate = 44100
+    player.play(sound, rate = rate)
+
+def pns(list_of_chords, t = 0.5, beat = 0):
+    """ Plays notes from a numpy array, where notes along the first dimension
+    are played sequentially and notes along the second dimension are played
+    simultaneously. "R" or "r" can be used to indicate rest.
+
+    t is the length of each note, in seconds.
+
+    Example: pns(np.asarray([["C4", "E4, "G4"], ["r, r, r"]["C4, "F4, "A4"]]))"""
+    if beat > 0:
+        list_of_chords = list_of_chords[beat - 1, :]
+    rate = 44100
+    s, Hz = sHz(rate)
+    n = 0
+    b = []
+    for chord in list_of_chords:
+        a = []
+        for note in chord:
+            if note.lower() == "r" or note == ".":
+                pass
+            else:
+                a.append(synth(freq(note)))
+        sample = zeros(int(t*s*n)).append(sum(a) * 0.5)
+        n += 1
+        b.append(sample)
+    delay_time = (t * n)
+    n += 4
+    sound = sum(b).take(int(t*s*n))
+    return sound, delay_time
+
+def song_sample_1():
+    track_1 = np.asarray(["C3 . . . G3 . . .".split()]).T
+    track_2 = np.asarray(["Eb4 . F4 Eb4 D4 . F4 Eb4".split()]).T
+    track_3 = np.asarray(["C5 G5 C6 G5 B6 D6 B6 G5".split()]).T
+    return [track_1, track_2, track_3]
 
 if __name__ == '__main__':
-    main()
-    rate = 44100 # Sampling rate, in samples/second
-    s, Hz = sHz(rate) # Seconds and hertz
-    ms = 1e-3 * s
-    start1 = 110
-    start2 = 220
-    multiplier = 2
-    for i in range(5):
-        note1 = karplus_strong(start1*pow(multiplier, i) * Hz) # Pluck "digitar" synth
-        note2 = zeros(300 * ms).append(karplus_strong(start2 * pow(multiplier, i) * Hz))
-        notes = (note1 + note2) * .5 #.5 is Amplitude
-        sound = notes.take(int(1.4 * s)) # 2 seconds of a Karplus-Strong note
-        with AudioIO(True) as player: # True means "wait for all sounds to stop"
-          player.play(sound, rate=rate)
+    print("Several functions to assist with realtime audio generation.")
