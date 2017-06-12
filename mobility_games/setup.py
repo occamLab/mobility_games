fb058dd56ef437ef4b20f7c57cd1e1a0670c379a
youralien <Ryan.Louie@students.olin.edu>
2017-06-12 12:40:08 -0400

    Handling multiple ros packages so mobility_sensing can be own package

--- mobility_games/setup.py
+++ mobility_games/setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['mobility_games'],
-    package_dir={'': '.'},
+    package_dir={'': '..'},
 )
 
 setup(**setup_args)
