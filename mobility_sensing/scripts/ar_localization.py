6035c50cdda59ed31bb6ea5a620b9999006cef25
youralien <Ryan.Louie@students.olin.edu>
2017-06-13 17:20:43 -0400

    Add 2 door tags test

---
+++ mobility_sensing/scripts/ar_localization.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import PoseStamped, PoseArray, Pose, Point
+from tf.transformations import quaternion_matrix
+
+
+class ARLocalization:
+
+    def __init__(self):
+        rospy.init_node('ar_localization')
+        rospy.Subscriber('/fisheye_undistorted/tag_detections_pose',
+                         PoseArray, self.republish_cane_tip)
+
+        self.pub = rospy.Publisher('/other_tag', PoseStamped, queue_size=1)
+
+        self.tag_distance = rospy.get_param('~tag_distance')
+
+    def republish_cane_tip(self, tag_msg):
+
+        # If any tags were detected
+        if len(tag_msg.poses):
+
+            # FIXME: Possibly poses[0] is not the correct one, if multiple tags
+            pos = tag_msg.poses[0].position
+            quat = tag_msg.poses[0].orientation
+
+            # rotation matrix way
+            # normal vector of tag is the z axis of the tag orientation
+            R = quaternion_matrix([quat.x, quat.y, quat.z, quat.w])
+
+            length_m = self.tag_distance
+
+            # FIXME: Tip location calc is approximate.
+            # Tag is not concentric with cane.
+
+            # normal was pointing towards handle
+            # opposite direction of normal is tip
+            cane_tip_position = Point(pos.x + length_m * R[0,2],
+                                      pos.y + length_m * R[1,2],
+                                      pos.z + length_m * R[2,2])
+
+            cane_tip_pose = Pose(position=cane_tip_position,
+                                 orientation=quat)
+
+            cane_tip_pose_stamped = PoseStamped(header=tag_msg.header,
+                                                pose=cane_tip_pose)
+
+            self.pub.publish(cane_tip_pose_stamped)
+
+    def run(self):
+        """ The main run loop"""
+        r = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            r.sleep()
+
+
+if __name__ == "__main__":
+    cane = ARLocalization()
+    cane.run()
