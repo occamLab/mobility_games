72de98769fd2853c0d6007f0f616fe838ba8b587
Jeremy <jeremychristopherryan@gmail.com>
2017-06-14 09:21:10 -0400

    Created musical cane game.

--- mobility_games/games/musical_cane.py
+++ mobility_games/games/musical_cane.py
@@ -9,6 +9,7 @@ from std_msgs.msg import Header, ColorRGBA
 import random
 from visualization_msgs.msg import Marker
 import pyttsx
+import os
 from os import system
 import numpy as np
 import pyaudio
@@ -16,21 +17,28 @@ import wave
 import time
 import sys
 from mobility_games.auditory.audio_controller import *
+from rospkg import RosPack
 
 class AudioFeedback(object):
     def __init__(self):
+        top = RosPack().get_path('mobility_games')
+        self.sound_folder = os.path.join(top, 'auditory/sound_files')
         rospy.init_node('audio_feedback')
         rospy.Subscriber('/tango_pose', PoseStamped, self.process_pose)
+        rospy.Subscriber('/cane_tip', PoseStamped, self.process_cane_tip)
         self.x = None   #   x and y position of Tango
         self.y = None
         self.yaw = None
+        self.cane_x = None
+        self.cane_y = None
+        self.cane_z = None
         self.start = False
         self.player = AudioIO(True)
-        self.tracks = song_sample_1()
-        self.quarter_time = 0.25
+        self.tracks = final_countdown()
+        self.quarter_time = 0.125
         self.track_length = len(self.tracks[0])
         self.track_time = self.quarter_time * self.track_length
-        self.music = arrays_to_sound(self.tracks, self.quarter_time)
+        self.music = arrays_to_sound(self.tracks, self.quarter_time, amps = [0.8, 1.0, 0.8, 1.2, 1.0, 0.6])
 
     def process_pose(self, msg):    #   Updates position of Tango
         self.x = msg.pose.position.x
@@ -42,39 +50,89 @@ class AudioFeedback(object):
                                         msg.pose.orientation.w])
         self.yaw = angles[2]
 
+    def process_cane_tip(self, msg):
+        #   Y is the cane's horizontal position, where y = 0 is the midline of the cane user.
+        self.cane_x = msg.pose.position.x
+        self.cane_y = msg.pose.position.y
+        self.cane_z = msg.pose.position.z
+        angles = euler_from_quaternion([msg.pose.orientation.x,
+                                        msg.pose.orientation.y,
+                                        msg.pose.orientation.z,
+                                        msg.pose.orientation.w])
+        self.yaw = angles[2]
+
     def run(self):
         lps = 3/self.quarter_time    #   loops per second!
         r = rospy.Rate(lps)
         beat = 1
         last_chord = rospy.Time.now()
+        print("Searching for Tango...")
         while not rospy.is_shutdown():
             print self.x
-            if self.x and not self.start:
+            print self.cane_y
+            if self.x and self.cane_x and not self.start:
                 #   Once position signals start to be received,
                 #   note initial position.
                 self.x_init = self.x
                 self.y_init = self.y
                 self.start = True
+                self.cane_y_prev = self.cane_y
                 print("Connection established.")
-            if self.x and self.start and rospy.Time.now() - last_chord >= rospy.Duration(self.quarter_time):
-                dist = math.sqrt((self.x - self.x_init)**2 + (self.y - self.y_init)**2)
+            if self.x and self.cane_x and self.start:
+                if self.cane_y * self.cane_y_prev <= 0:
+                    system("aplay " + os.path.join(self.sound_folder, "beep.wav"))
+                    print("beep")
+                self.cane_y_prev = self.cane_y
+            r.sleep()
+
+    def run_music(self):
+        lps = 3/self.quarter_time    #   loops per second!
+        r = rospy.Rate(lps)
+        print lps
+        beat = 1
+        last_chord = rospy.Time.now()
+        print("Searching for Tango...")
+        while not rospy.is_shutdown():
+            print self.x
+            print self.cane_y
+            if self.x and self.cane_x and not self.start:
+                #   Once position signals start to be received,
+                #   note initial position.
+                self.x_init = self.x
+                self.y_init = self.y
+                self.start = True
+                self.cane_y_prev = self.cane_y
+                playing = 0
+                print("Connection established.")
+                last_sweep = rospy.Time.now()
+            if self.x and self.cane_x and self.start:
+                if rospy.Time.now() - last_sweep > rospy.Duration(1.5):
+                    playing = 0
+            if self.x and self.cane_x and self.start:
+                if self.cane_y * self.cane_y_prev <= 0:
+                    playing = 1
+                    last_sweep = rospy.Time.now()
+                if self.cane_y_prev != self.cane_y:
+                    self.cane_y_prev = self.cane_y
+            if self.x and self.cane_x and self.start and rospy.Time.now() - last_chord > rospy.Duration(self.quarter_time) and playing:
                 #   Play variable numbers of tracks depending on distance from
                 #   initial position.
-                if dist < 1:
+
+                chord = chord = sum(self.music[beat - 1, :])
+                """if dist < 1:
                     chord = sum(self.music[beat - 1, :])
                 elif dist < 2:
                     chord = sum(self.music[beat - 1, :-1])
                 else:
-                    chord = sum(self.music[beat - 1, :-2])
+                    chord = sum(self.music[beat - 1, :-2])"""
                 play(chord, self.player)
                 print(chord)
                 beat += 1
                 if beat > self.track_length:
                     beat = 1
-                    print("beat")
                 last_chord = rospy.Time.now()
             r.sleep()
 
 if __name__ == '__main__':
     node = AudioFeedback()
-    node.run()
+    node.run_music()
