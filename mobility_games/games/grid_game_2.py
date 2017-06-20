a31e90aff6d27eaa7e664f97bb02166edb5f75ae
smalhi5 <simmalhi123@gmail.com>
2017-06-20 14:09:54 -0600

    Added comment to the programs

--- mobility_games/games/grid_game_2.py
+++ mobility_games/games/grid_game_2.py
@@ -40,6 +40,10 @@ class Turns(object):
         self.angleList = [90,-90,180] 
         self.hasSpoken = False
 
+    """
+        This function gets the x, y, z posiition
+        This function also gets the yaw which is z rotation
+    """
     def process_pose(self, msg):
         self.x = msg.pose.position.x
         self.y = msg.pose.position.y
@@ -50,9 +54,17 @@ class Turns(object):
                                         msg.pose.orientation.w])
         self.yaw = angles[2]
 
+    """
+        This function get the dist from plane_finder file
+    """
     def process_dist(self,msg):
         self.dist = msg.data
 
+    """
+        This function is where you turn according z rotation
+        according to the degree you have turn play onve
+        you hit the goal
+    """
     def turn_game(self,angle):
         self.yaw = None  
         sound = {90:"Turn Left", -90:"Turn right", 180:"Turn around"}
@@ -63,56 +75,69 @@ class Turns(object):
             self.engine.say(speech)
             self.engine.runAndWait()
         self.hasSpoken = True
-        r = rospy.Rate(10)
-        while not self.yaw:  
+        r = rospy.Rate(10) #set the loop rate
+        while not self.yaw: #not reieveing data from streamer 
             print self.yaw  
             r.sleep()
             pass
+        #calculate the targeted angle
         goal_angle = self.yaw + angle*math.pi/180
-        goal_angle %= 2*math.pi
-        threshold = 20*math.pi/180
+        goal_angle %= 2*math.pi #puts it in the form of 2pi
+        threshold = 20*math.pi/180 #angle thresh hold 
         while not rospy.is_shutdown():
+            #Once you are in the angle thresh hold play the sound
             if abs(angle_diff(self.yaw, goal_angle)) <= threshold: #Make sure if you hit the threshhold
                 self.jumpNoise.play()
                 self.lastJumpNoise = rospy.Time.now()
                 break  
             r.sleep()
 
+    """
+        This function make you walk in a straight line
+        play sound if you are in a straight line
+        once you have reach you gaol distacne 
+        it maek you turn in a random directio 
+    """
     def straight(self):
         self.x = None
         self.y = None
         self.yaw = None
         self.dist = None
-        r = rospy.Rate(10)
-        while not self.yaw: 
+        r = rospy.Rate(10) #set the rate for loops
+        while not self.yaw: #not recieving data from streamer
             print "NONE"
             r.sleep()
             pass
-        while not self.x:  
+        while not self.x: #not recieving data from streamer
             print "None"
             r.sleep()
             pass
         straightAngle = self.yaw
         errorAngle = 15*math.pi/180
-        current_x = self.x
-        current_y = self.y
-        while not rospy.is_shutdown():
+        pos_x = self.x #gets the x position before you start moving
+        pos_y = self.y #gets the y position before you start moving
+        while not rospy.is_shutdown(): #tmake sure rospy not shutdown
+            #threshhold of the stragith angle 
             if abs(angle_diff(self.yaw, straightAngle)) < errorAngle:
+                #play the sound if you are in a straigth line
                 if rospy.Time.now() - self.lastBeepNoise > rospy.Duration(1): 
                     self.beepNoise.close()
                     self.beepNoise = pw.Wav(path.join(self.sound_folder, "beep3.wav"))
                     self.beepNoise.play()
                     self.lastBeepNoise = rospy.Time.now()
-                x = self.x - current_x
-                y = self.y - current_y
+                x = self.x - pos_x
+                y = self.y - pos_y
+                #calculate the distance between current posiiton to the starting position
                 distance = math.sqrt((x**2)+(y**2))
                 print("Wall Distance: " + str(self.dist))
+                #If a wall is within 1.5 meter make you turn
                 if self.dist < 1.5:
                     degree = random.choice(self.angleList)  
                     self.turn_game(degree)
-                    current_y = self.y
-                    current_x = self.x
+                    pos_y = self.y
+                    pos_x = self.x
                     straightAngle = self.yaw
+                #Once you walk 1.5 meter makes you turn
                 if distance >= 1.5:  
                     self.dingNoise.play()
                     self.lastDingNoise = rospy.Time.now()
@@ -124,6 +149,9 @@ class Turns(object):
                     break  
             r.sleep()
 
+    """
+        Thisfunction just run the program
+    """
     def run(self):
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
@@ -134,7 +162,9 @@ class Turns(object):
             r.sleep()
 
 
-
+"""
+    The Main function 
+"""
 if __name__ == '__main__':
     node = Turns()
     node.run()
