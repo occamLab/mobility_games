3c8eb35b9e32be6fe864e1b920a9c9b4405fa1ca
smalhi5 <simmalhi123@gmail.com>
2017-06-26 08:50:22 -0600

    Added Dynamic Config to GreidGame2 parem the distance traveled and distance from the wall

--- mobility_games/games/grid_game_2.py
+++ mobility_games/games/grid_game_2.py
@@ -21,6 +21,9 @@ from rospkg import RosPack
 import pcl
 from sensor_msgs.msg import PointCloud
 from threading import Lock
+from mobility_games.cfg import GridGame2Config
+from dynamic_reconfigure.server import Server
+
 
 class Turns(object):
 
@@ -28,6 +31,7 @@ class Turns(object):
         rospy.init_node("grid_game_2")
         rospy.Subscriber("/tango_pose", PoseStamped,self.process_pose)
         rospy.Subscriber("/wall_dist", Float64, self.process_dist)
+        srv = Server(GridGame2Config, self.config_callback)
         top = RosPack().get_path('mobility_games') 
         self.sound_folder = path.join(top, 'auditory/sound_files')
         self.engine = pyttsx.init() 
@@ -37,7 +41,9 @@ class Turns(object):
         self.lastDingNoise = rospy.Time.now() 
         self.lastJumpNoise = rospy.Time.now() 
         self.lastBeepNoise = rospy.Time.now() 
-        self.angleList = [90,-90,180] 
+        self.angleList = [90,-90,180]
+        self.traveled_distance = rospy.get_param('~traveled_distance', 2.0)
+        self.wall_threshold = rospy.get_param('~wall_threshold', 1.5)
         self.hasSpoken = False
 
     """
@@ -59,6 +65,12 @@ class Turns(object):
     """
     def process_dist(self,msg):
         self.dist = msg.data
+
+    def config_callback(self, config, level):
+        self.traveled_distance = config['pc_traveledDistance']
+        self.wall_threshold = config['pc_distanceFromWall']
+
+        return config
 
     """
         This function is where you turn according z rotation
@@ -129,17 +141,8 @@ class Turns(object):
                 y = self.y - pos_y
                 #calculate the distance between current posiiton to the starting position
                 distance = math.sqrt((x**2)+(y**2))
-                print("Wall Distance: " + str(self.dist))
-                #If a wall is within 1.5 meter make you turn
-                if self.dist < 1.5 and self.dist > 0:
-                    degree = random.choice(self.angleList)  
-                    if rospy.Time.now() - self.lastJumpNoise > rospy.Duration(2):
-                        self.jumpNoise.close()
-                        self.jumpNoise = pw.Wav(path.join(self.sound_folder, "jomp.wav"))
-                        self.turn_game(degree)  
-                    break 
                 #Once you walk 1.5 meter makes you turn
-                if distance >= 2:  
+                if distance >= self.traveled_distance:  
                     self.dingNoise.play()
                     self.lastDingNoise = rospy.Time.now()
                     degree = random.choice(self.angleList)  
@@ -147,7 +150,15 @@ class Turns(object):
                         self.jumpNoise.close()
                         self.jumpNoise = pw.Wav(path.join(self.sound_folder, "jomp.wav"))
                         self.turn_game(degree)  
-                    break  
+                    break
+            #If a wall is within 1.5 meter make you turn
+            if self.dist < self.wall_threshold and self.dist > 0:
+                degree = random.choice(self.angleList)  
+                if rospy.Time.now() - self.lastJumpNoise > rospy.Duration(2):
+                    self.jumpNoise.close()
+                    self.jumpNoise = pw.Wav(path.join(self.sound_folder, "jomp.wav"))
+                    self.turn_game(degree)  
+                break   
             r.sleep()
 
     """
