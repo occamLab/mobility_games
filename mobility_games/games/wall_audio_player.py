b182c7c150a47a9e082f15ee56527430759256aa
juicyslew <william.derksen@students.olin.edu>
2017-06-26 13:21:50 -0400

    Wall Audio Update

--- mobility_games/games/wall_audio_player.py
+++ mobility_games/games/wall_audio_player.py
@@ -19,54 +19,67 @@ class wall_audio_player(object):
         self.dingsound = path.join(top, 'auditory/sound_files/ding.wav')
         srv = Server(WallAudioConfig, self.config_callback)
         #self.gmode = rospy.get_param('~pc_audio_gmode', 'pvr')
-        self.pitch = rospy.get_param('~pc_pitch', True)
-        self.vol = rospy.get_param('~pc_vol', True)
-        self.speed = rospy.get_param('~pc_rate', True)
-        self.track = rospy.get_param('~pc_track', False)
-        self.ding = rospy.get_param('~pc_ding', False)
-        self.randrange = rospy.get_param('~pc_randrange', 1);
-        self.quantizetype = rospy.get_param('~pc_quanttype', 'dom7')
-        self.key = rospy.get_param('~pc_key', 'A')
+        self.pitch = rospy.get_param('~pitch', True)
+        self.pitchdefault = rospy.get_param('~pitchDefault', 'A4')
+        self.vol = rospy.get_param('~vol', True)
+        self.voldefault = rospy.get_param('~volDefault', .8)
+        self.speed = rospy.get_param('~rate', True)
+        self.speeddefault = rospy.get_param('~rateDefault', 20);
+        self.track = rospy.get_param('~track', False)
+        self.ding = rospy.get_param('~ding', False)
+        self.randrange = rospy.get_param('~randRange', 0);
+        self.quantizetype = rospy.get_param('~quantType', 'M')
+        self.key = rospy.get_param('~key', 'A')
+        self.on = rospy.get_param('~on', False)
+        self.altsounddist = rospy.get_param('~successZoneSize', .5)
+        self.maxfreqdist = 5.0;
         self.last_sound_time = rospy.Time.now() #Initialize the last time a sound was made
         self.dist = None
 
         self.player = ac.player() #play the sound.
-        self.altsounddist = .5;
-        self.rate = 1.0/20.0;
+        self.rate = 1.0/self.speeddefault;
         self.r = rospy.Rate(1.0/self.rate);
+        self.dingHappened = False;
 
     def config_callback(self, config, level):
-        self.pitch = config['pc_pitch']
-        self.vol = config['pc_vol']
-        self.speed = config['pc_rate']
-        self.track = config['pc_track']
-        self.ding = config['pc_ding']
-        self.quantizetype = config['pc_quanttype']
-        self.key = config['pc_key']
-        self.randrange = config['pc_randrange']
+        self.pitch = config['pitch']
+        self.vol = config['vol']
+        self.speed = config['rate']
+        self.track = config['track']
+        self.ding = config['ding']
+        self.quantizetype = config['quantType']
+        self.key = config['key']
+        self.randrange = config['randRange']
+        self.on = config['on']
+        self.pitchdefault = config['pitchDefault']
+        self.voldefault = config['volDefault']
+        self.speeddefault = config['rateDefault']
         return config
 
     def get_dist(self, msg):
         self.dist = msg.data
+        print(self.dist)
         if self.speed:
             filtereddist =  max(min(3*self.dist, 20), 2)
             self.rate = 1.0/filtereddist;
             self.r = rospy.Rate(1.0/self.rate)
         else:
-            pass
+            self.rate = 1.0/self.speeddefault;
+            self.r = rospy.Rate(self.speeddefault)
+
     def run(self):
         #self.r =  #Attempts to run at a rate of 10 times a second (although never reaches this speed)
         #self.dist = 0
-        rewardsound=pw.Wav(self.dingsound)
         while not rospy.is_shutdown(): #Start main while loop
             #self.dist = random.random()*4
-            if not self.dist is None:
+            if not self.dist is None and self.on:
                 #if rospy.Time.now()-self.last_sound_time > rospy.Duration(.01) and self.walldist is not 0: #If it's been longer than 2 seconds since last sound, and the wall distance is greater than half a meter
                 self.last_sound_time = rospy.Time.now() #reset sound time
                 if abs(self.dist) < self.altsounddist and self.ding:
-                    rewardsound.close()
                     rewardsound=pw.Wav(self.dingsound)
                     rewardsound.play()
+                    self.dingHappened = True;
+                    #print('dinged')
 
                 else:
                     freq = ac.freq('C5')
@@ -91,8 +104,13 @@ class wall_audio_player(object):
                         #else:
                         freq = ac.quantize(freq, quantizetype = self.quantizetype, key=self.key)
                         #print (freq)
+                    else:
+                        if (self.pitchdefault != ''):
+                            freq = ac.freq(self.pitchdefault)
                     if (self.vol):
                         vol = (.8/(1+math.exp((self.dist)/1.5-1))+.2)*vol#insert volume function here. that goes from 1 to 0 with increasing distance to wall.
+                    else:
+                        vol = self.voldefault
                     #self.synth = ac.delay(self.synth, 1, self.s*.2, .5) #add delay to the synth
 
                     if self.track:
@@ -103,6 +121,10 @@ class wall_audio_player(object):
                         ac.playstream(synth, self.player, seconds = seconds, volume = vol)#synth, self.player, seconds = .5, volume = vol)
             #print(walldistself.dist)
             self.r.sleep() #wait until next iteration
+            if (self.dingHappened):
+                self.dingHappened = False
+                rewardsound.close()
+
 
 if __name__ == '__main__': #Run Code
     node = wall_audio_player()
